5eee0e95daf70193a8146cdd7a4531b0435fa277
Krushovice <kickstar69@yandex.ru>
2025-05-23 16:55:32 +1000

    update database config

--- src/core/config.py
+++ src/core/config.py
@@ -3,8 +3,6 @@ from pathlib import Path
 from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 
 class AppConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -13,24 +11,32 @@ class AppConfig(BaseModel):
 
 
 class DBConfig(BaseModel):
-    url: str = PostgresDsn
+    url: PostgresDsn
 
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=f"{BASE_DIR}.env",
-        env_prefix="CONFIG",
+        env_file=".env",
+        env_prefix="CONFIG__",
         env_nested_delimiter="__",
         env_file_encoding="utf-8",
     )
 
     app: AppConfig = AppConfig()
-    # db: DBConfig
+    db: DBConfig
 
 
 settings = Settings()
