a31d436850b60ddec8861fd799aae8b01e4b11b9
Krushovice <kickstar69@yandex.ru>
2025-05-28 20:33:10 +1000

    create login&register templates

---
+++ src/api/films/forms.py
@@ -0,0 +1,2 @@
+class FilmForm:
+    pass
---
+++ src/core/models/user.py
@@ -0,0 +1,8 @@
+from core.models import Base
+from core.models.mixins.id_int_pk import IdIntPkMixin
+
+from fastapi_users.db import SQLAlchemyBaseUserTable
+
+
+class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
+    pass
