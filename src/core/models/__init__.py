61dbf7b4d83aaa3ae423301ce481d68b1b7ba429
Krushovice <kickstar69@yandex.ru>
2025-05-29 17:34:52 +1000

    create access token model

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -4,6 +4,7 @@ __all__ = (
     "Genre",
     "FilmGenreAssoc",
     "User",
+    "AccessToken",
 )
 
 from .base import Base
@@ -11,3 +12,4 @@ from .film import Film
 from .genre import Genre
 from .film_genre_assoc import FilmGenreAssoc
 from .user import User
+from .access_token import AccessToken
