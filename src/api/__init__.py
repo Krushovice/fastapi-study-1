78d1226c2eb0381403479ec19854a9c53208b2cc
Krushovice <kickstar69@yandex.ru>
2025-05-29 20:04:17 +1000

    create auth router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -7,9 +7,13 @@ from fastapi import APIRouter
 
 from api.films.router import router as films_router
 from api.users.router import router as users_router
+from .auth_router import router as auth_router
+
 from api.base_crud import BaseCRUD
 
 router = APIRouter()
 
 router.include_router(films_router)
 router.include_router(users_router)
+
+router.include_router(auth_router)
