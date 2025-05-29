3f2115c13bfe7bf3c7066a805225311d9ac17d1b
Krushovice <kickstar69@yandex.ru>
2025-05-29 20:11:00 +1000

    include auth router from fastapi users

--- src/api/auth_router.py
+++ src/api/auth_router.py
@@ -1,7 +1,14 @@
 from fastapi import APIRouter
 
+from .dependencies.authentification.backend import authentication_backend
+from .fastapi_users_routers_helper import fastapi_users
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"],
+)
+
+
+router.include_router(
+    router=fastapi_users.get_auth_router(authentication_backend),
 )
