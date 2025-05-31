183755cfd383bf6492cccd8197498e01c85bf781
Krushovice <kickstar69@yandex.ru>
2025-05-31 16:15:12 +1000

    include register router

--- src/api/auth_router.py
+++ src/api/auth_router.py
@@ -2,13 +2,24 @@ from fastapi import APIRouter
 
 from .dependencies.authentification.backend import authentication_backend
 from .fastapi_users_routers_helper import fastapi_users
+from .users.schemas import UserRead, UserCreate
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"],
 )
 
-
+# /login /logout
 router.include_router(
-    router=fastapi_users.get_auth_router(authentication_backend),
+    router=fastapi_users.get_auth_router(
+        authentication_backend,
+    ),
+)
+
+# /register
+router.include_router(
+    fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
 )
