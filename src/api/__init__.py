86e688b1be86561a12d48c34e54210fa888762fb
Krushovice <kickstar69@yandex.ru>
2025-06-01 16:57:28 +1000

    add http bearer dependency for docs

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,7 +3,8 @@ __app__ = (
     "BaseCRUD",
 )
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from api.films.router import router as films_router
 from api.users.router import router as users_router
@@ -12,7 +13,11 @@ from .auth_router import router as auth_router
 
 from api.base_crud import BaseCRUD
 
-router = APIRouter(prefix=settings.api.prefix)
+http_bearer = HTTPBearer(auto_error=False)
+router = APIRouter(
+    prefix=settings.api.prefix,
+    dependencies=[Depends(http_bearer)],
+)
 
 router.include_router(films_router)
 router.include_router(users_router)
