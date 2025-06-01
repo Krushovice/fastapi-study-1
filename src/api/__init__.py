8f6c8eaa9983d34f1b519523837c8478fb6b647f
Krushovice <kickstar69@yandex.ru>
2025-06-01 16:49:05 +1000

    add api prefix

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -7,11 +7,12 @@ from fastapi import APIRouter
 
 from api.films.router import router as films_router
 from api.users.router import router as users_router
+from core import settings
 from .auth_router import router as auth_router
 
 from api.base_crud import BaseCRUD
 
-router = APIRouter()
+router = APIRouter(prefix=settings.api.prefix)
 
 router.include_router(films_router)
 router.include_router(users_router)
